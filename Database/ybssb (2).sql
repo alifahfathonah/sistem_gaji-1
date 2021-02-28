d4579f92e10a3eee10a6ab2d65afbb15154dc7bb
Fitra Arrafiq <fitraarrafiq@gmail.com>
2021-02-28 08:55:45 +0700

    update database

--- Database/ybssb (2).sql
+++ Database/ybssb (2).sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 01, 2021 at 01:12 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 7.0.0
+-- Host: localhost
+-- Generation Time: Feb 28, 2021 at 08:54 AM
+-- Server version: 5.7.33-0ubuntu0.18.04.1
+-- PHP Version: 7.1.33-29+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -48,17 +49,18 @@ CREATE TABLE `bonus_kinerja` (
   `tanggal` date NOT NULL,
   `nilai_kpi` text NOT NULL,
   `jumlah_bonus` text NOT NULL,
-  `total_gaji` text NOT NULL
+  `total_gaji` text NOT NULL,
+  `approve_yayasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bonus_kinerja`
 --
 
-INSERT INTO `bonus_kinerja` (`id`, `id_karyawan`, `tanggal`, `nilai_kpi`, `jumlah_bonus`, `total_gaji`) VALUES
-(1, '39', '2021-01-07', '20', '160000', '960000'),
-(2, '30', '2021-01-06', '15', '210000', '1610000'),
-(4, '37', '2021-01-07', '10', '80000', '880000');
+INSERT INTO `bonus_kinerja` (`id`, `id_karyawan`, `tanggal`, `nilai_kpi`, `jumlah_bonus`, `total_gaji`, `approve_yayasan`) VALUES
+(1, '39', '2021-01-07', '20', '160000', '960000', 0),
+(2, '30', '2021-01-06', '15', '210000', '1610000', 0),
+(4, '37', '2021-01-07', '10', '80000', '880000', 1);
 
 -- --------------------------------------------------------
 
@@ -70,15 +72,16 @@ CREATE TABLE `bonus_lebaran` (
   `id` int(11) NOT NULL,
   `id_karyawan` text NOT NULL,
   `tanggal` date NOT NULL,
-  `total_gaji_bonus` text NOT NULL
+  `total_gaji_bonus` text NOT NULL,
+  `approve_yayasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bonus_lebaran`
 --
 
-INSERT INTO `bonus_lebaran` (`id`, `id_karyawan`, `tanggal`, `total_gaji_bonus`) VALUES
-(17, '39', '2021-01-06', '800000');
+INSERT INTO `bonus_lebaran` (`id`, `id_karyawan`, `tanggal`, `total_gaji_bonus`, `approve_yayasan`) VALUES
+(17, '39', '2021-01-06', '800000', 1);
 
 -- --------------------------------------------------------
 
@@ -97,68 +100,43 @@ CREATE TABLE `gaji_bulanan` (
   `uang_lembur` text NOT NULL,
   `bonus_lain` text NOT NULL,
   `total_gaji` text NOT NULL,
-  `total_potongan` text NOT NULL
+  `total_potongan` text NOT NULL,
+  `approve_yayasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `gaji_bulanan`
 --
 
-INSERT INTO `gaji_bulanan` (`id`, `id_karyawan`, `tanggal`, `uang_transport`, `tunjangan_kinerja`, `tunjangan_jabatan`, `uang_extra_kurikuler`, `uang_lembur`, `bonus_lain`, `total_gaji`, `total_potongan`) VALUES
-(2, '11', '2021-01-07', '0', '50000', '0', '0', '0', '0', '1450000', '0'),
-(3, '14', '2021-01-07', '0', '0', '0', '0', '0', '100000', '1500000', '0'),
-(4, '17', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(5, '19', '2021-01-07', '0', '0', '0', '0', '0', '100000', '1500000', '0'),
-(6, '20', '2021-01-29', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(7, '22', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(8, '23', '2021-01-07', '0', '0', '0', '0', '0', '0', '1000000', '0'),
-(10, '25', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(11, '26', '2021-01-07', '0', '0', '0', '0', '0', '100000', '200000', '0'),
-(12, '27', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(13, '28', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(14, '29', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(15, '30', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(16, '31', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(17, '32', '2021-01-01', '0', '0', '0', '0', '0', '50000', '1450000', '0'),
-(18, '33', '2021-01-07', '0', '0', '0', '0', '0', '45000', '1445000', '0'),
-(25, '39', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(26, '38', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(27, '37', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(29, '35', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(30, '34', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(31, '18', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(32, '18', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(33, '16', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(34, '15', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(35, '13', '2021-01-07', '0', '0', '0', '0', '0', '0', '2000000', '0'),
-(36, '31', '2021-01-12', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(39, '39', '2021-02-24', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(40, '38', '2021-02-24', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(41, '37', '2021-02-24', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(42, '35', '2021-02-24', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(43, '34', '2021-02-24', '0', '0', '0', '0', '0', '0', '800000', '0'),
-(44, '33', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(45, '32', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(46, '31', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(47, '30', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(48, '29', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(49, '28', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(50, '27', '2021-02-24', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(51, '26', '2021-02-24', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(52, '25', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(53, '24', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(54, '23', '2021-02-24', '0', '0', '0', '0', '0', '0', '1000000', '0'),
-(55, '22', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(56, '21', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(57, '20', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(58, '19', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(59, '18', '2021-02-24', '0', '0', '0', '0', '0', '0', '100000', '0'),
-(60, '17', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(61, '16', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(62, '15', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(63, '14', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0'),
-(64, '13', '2021-02-24', '0', '0', '0', '0', '0', '0', '2000000', '0'),
-(65, '11', '2021-02-24', '0', '0', '0', '0', '0', '0', '1400000', '0');
+INSERT INTO `gaji_bulanan` (`id`, `id_karyawan`, `tanggal`, `uang_transport`, `tunjangan_kinerja`, `tunjangan_jabatan`, `uang_extra_kurikuler`, `uang_lembur`, `bonus_lain`, `total_gaji`, `total_potongan`, `approve_yayasan`) VALUES
+(2, '11', '2021-01-07', '0', '50000', '0', '0', '0', '0', '1450000', '0', 0),
+(3, '14', '2021-01-07', '0', '0', '0', '0', '0', '100000', '1500000', '0', 0),
+(4, '17', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(5, '19', '2021-02-11', '0', '0', '0', '0', '0', '100000', '1500000', '0', 0),
+(6, '20', '2021-01-29', '0', '0', '0', '0', '0', '0', '1400000', '0', 0),
+(7, '22', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(8, '23', '2021-01-07', '0', '0', '0', '0', '0', '0', '1000000', '0', 0),
+(10, '25', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(11, '26', '2021-01-07', '0', '0', '0', '0', '0', '100000', '200000', '0', 1),
+(12, '27', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0', 0),
+(13, '28', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(14, '29', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0', 0),
+(15, '30', '2021-01-07', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(16, '31', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0', 0),
+(17, '32', '2021-01-01', '0', '0', '0', '0', '0', '50000', '1450000', '0', 0),
+(18, '33', '2021-01-07', '0', '0', '0', '0', '0', '45000', '1445000', '0', 0),
+(25, '39', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0', 0),
+(26, '38', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0', 0),
+(27, '37', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0', 0),
+(29, '35', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0', 0),
+(30, '34', '2021-01-07', '0', '0', '0', '0', '0', '0', '800000', '0', 0),
+(31, '18', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0', 0),
+(32, '18', '2021-01-07', '0', '0', '0', '0', '0', '0', '100000', '0', 0),
+(33, '16', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0', 1),
+(34, '15', '2021-01-07', '0', '0', '0', '0', '0', '0', '1400000', '0', 1),
+(35, '13', '2021-01-07', '0', '0', '0', '0', '0', '0', '2000000', '0', 1),
+(36, '31', '2021-01-12', '0', '0', '0', '0', '0', '0', '1400000', '0', 1),
+(38, '41', '2021-01-24', '0', '0', '0', '0', '0', '0', '1400000', '0', 1);
 
 -- --------------------------------------------------------
 
@@ -227,15 +205,16 @@ CREATE TABLE `guru_terbaik` (
   `upload_portofolio` text NOT NULL,
   `keterangan` text NOT NULL,
   `jumlah_bonus` text NOT NULL,
-  `total_gaji` text NOT NULL
+  `total_gaji` text NOT NULL,
+  `approve_yayasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `guru_terbaik`
 --
 
-INSERT INTO `guru_terbaik` (`id`, `id_karyawan`, `tanggal`, `upload_portofolio`, `keterangan`, `jumlah_bonus`, `total_gaji`) VALUES
-(12, '39', '2021-01-06', 'gntg.jpg', 'Menjadi Guru SD Terbaik se-pekanbaru', '50000', '850000');
+INSERT INTO `guru_terbaik` (`id`, `id_karyawan`, `tanggal`, `upload_portofolio`, `keterangan`, `jumlah_bonus`, `total_gaji`, `approve_yayasan`) VALUES
+(12, '39', '2021-01-06', 'gntg.jpg', 'Menjadi Guru SD Terbaik se-pekanbaru', '50000', '850000', 1);
 
 -- --------------------------------------------------------
 
@@ -317,7 +296,7 @@ INSERT INTO `karyawan` (`id_karyawan`, `role`, `nama_karyawan`, `tgl_lahir`, `jk
 (24, '1', 'DESI SURYA TUTI, S.Pd', '1990-12-12', 'PR', '', '', 'Jl. Sukajadi, Blok G.14', '8', 6, 'Pend. Matematika', 'UIN SUSKA', 'S1', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-06'),
 (25, '1', 'AMI SABRINA', '1995-05-12', 'PR', '', '', 'Jl. Soebrantas Perum.Villa Pesona Panam. B. 09', '8', 6, 'Pend. Kimia', 'UIN SUSKA', 'S1', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-06'),
 (26, '1', 'DEDE KURNIA ISLAMI,S.Pi', '1995-06-10', 'LK', '', '', 'Jl. Pepaya Gg. Tanjung', '16', 4, 'Perikanan', 'Universitas Riau', 'S1', '', 'Aktif', '', '21', '100000', '100000', '2021-01-06'),
-(27, '1', 'MULYATI,A.Md', '0000-00-00', 'PR', '', '', 'JL. Taman Karya', '11', 4, '', '', '', '', 'Aktif', '', '21', '100000', '100000', '2021-01-07'),
+(27, '1', 'MULYATI,A.Md', '0000-00-00', 'PR', '', '', 'JL. Taman Karya', '11', 4, '', '', '', '', 'Aktif', '', '21', '102000', '102000', '2021-01-07'),
 (28, '1', 'NUH ASEP SATRIA,S.Pd', '1995-10-12', 'LK', '', '', 'Rimbo Panjang, Kampar', '15', 6, 'P.B.Arab', 'UIN SUSKA', 'S1', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-07'),
 (29, '1', 'MASDA GUSTINAH HSB,M.Pd', '1995-08-17', 'PR', '', '', 'Perum Grentari Blok G NO.2', '8', 6, 'Agama Islam', 'UIN SUSKA', 'S2', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-07'),
 (30, '1', 'MAYANG SARI NAINGGOLAN,S.Pd', '1996-07-12', 'PR', '', '', 'Jl. Bangun Karya Gg. Bangun III', '8', 6, 'Pend. Matematika', 'Universitas Riau', 'S1', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-07'),
@@ -328,7 +307,9 @@ INSERT INTO `karyawan` (`id_karyawan`, `role`, `nama_karyawan`, `tgl_lahir`, `jk
 (35, '1', 'ANDI SANAFIYAH', '0000-00-00', 'LK', '', '', '', '18', 3, '', '', '', '', 'Aktif', '', '22', '800000', '800000', '2021-01-07'),
 (37, '1', 'SOEKARNO PONIMIN', '0000-00-00', 'LK', '', '', '', '18', 3, '', '', '', '', 'Tidak Aktif', '', '22', '800000', '800000', '2021-01-07'),
 (38, '1', 'AHMAD', '0000-00-00', 'LK', '', '', '', '17', 3, '', '', '', '', 'Tidak Aktif', '', '22', '800000', '800000', '2021-01-07'),
-(39, '1', 'RULI UWIR LIANTO', '0000-00-00', 'LK', '', '', '', '17', 3, '', '', '', '', 'Tidak Aktif', '', '22', '800000', '800000', '2021-01-07');
+(39, '1', 'RULI UWIR LIANTO', '0000-00-00', 'LK', '', '', '', '17', 3, '', '', '', '', 'Tidak Aktif', '', '22', '800000', '800000', '2021-01-07'),
+(40, '1', 'uun', '2021-01-07', 'LK', '', '', 'jl. tegal sari', '13', 6, '', '', '', '', 'Aktif', '', '13', '1400000', '1400000', '2021-01-22'),
+(41, '1', 'Agus hidup', '2021-01-23', 'LK', 'agus@gmail.com', '0823122333', 'Medan, Pekanbaru', '9', 6, '', '', '', '', '', '', '13', '1400000', '1400000', '2021-01-23');
 
 -- --------------------------------------------------------
 
@@ -341,8 +322,16 @@ CREATE TABLE `kenaikan_gaji` (
   `id_karyawan` text NOT NULL,
   `persentase` text NOT NULL,
   `jumlah_kenaikan` text NOT NULL,
-  `total_gaji` text NOT NULL
+  `total_gaji` text NOT NULL,
+  `approve_yayasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kenaikan_gaji`
+--
+
+INSERT INTO `kenaikan_gaji` (`id`, `id_karyawan`, `persentase`, `jumlah_kenaikan`, `total_gaji`, `approve_yayasan`) VALUES
+(1, '27', '2', '2000', '102000', 0);
 
 -- --------------------------------------------------------
 
@@ -405,9 +394,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `email`, `role`, `block_status`, `online_status`, `time_online`, `time_offline`) VALUES
-(54, 'DEDE KURNIA ISLAMI,S.Pi', '', 'superadmin', '9c1d9390a6bde8e38e32775521d7c406', '', 'administrator', 0, 'offline', '2021-01-14 15:57:47', '2021-01-14 15:57:47'),
-(55, 'JUSNITA, SE', '', 'keuangan', '707f2ab2fe96b61ea294097eaddb3af3', '', 'keuangan', 0, 'offline', '2021-01-21 20:09:36', '2021-01-21 20:09:36'),
-(56, 'WAHYU ISKANDAR,S.Pd.I', '', 'yayasan', 'aa8257f900dee375589637d4fa89b08f', '', 'yayasan', 0, 'online', '2021-01-27 00:01:57', '2021-01-27 00:01:57');
+(54, 'DEDE KURNIA ISLAMI,S.Pi', '', 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', '', 'administrator', 0, 'offline', '2021-02-27 16:58:34', '2021-02-27 16:58:34'),
+(55, 'JUSNITA, SE', '', 'keuangan', '87cbf810625de2ff054ac8b841e135df', '', 'keuangan', 0, 'online', '2021-02-28 00:27:57', '2021-02-28 00:27:57'),
+(56, 'WAHYU ISKANDAR,S.Pd.I', '', 'yayasan', 'e62f7824c7aab9dfc2d8ca00448afbfa', '', 'yayasan', 0, 'online', '2021-02-28 00:26:09', '2021-02-28 00:26:09');
 
 -- --------------------------------------------------------
 
@@ -534,71 +523,86 @@ ALTER TABLE `users1`
 --
 ALTER TABLE `absensi`
   MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `bonus_kinerja`
 --
 ALTER TABLE `bonus_kinerja`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `bonus_lebaran`
 --
 ALTER TABLE `bonus_lebaran`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- AUTO_INCREMENT for table `gaji_bulanan`
 --
 ALTER TABLE `gaji_bulanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
 --
 -- AUTO_INCREMENT for table `gaji_tambahan`
 --
 ALTER TABLE `gaji_tambahan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `golongan`
 --
 ALTER TABLE `golongan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
 --
 -- AUTO_INCREMENT for table `guru_terbaik`
 --
 ALTER TABLE `guru_terbaik`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
 --
 -- AUTO_INCREMENT for table `kenaikan_gaji`
 --
 ALTER TABLE `kenaikan_gaji`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `tingkat_jabatan`
 --
 ALTER TABLE `tingkat_jabatan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `transaksi_gaji_tambahan`
 --
 ALTER TABLE `transaksi_gaji_tambahan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
 --
 -- AUTO_INCREMENT for table `users1`
 --
 ALTER TABLE `users1`
   MODIFY `id_users` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
